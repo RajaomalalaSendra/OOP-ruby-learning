f9b7565b657f40a31a8473047549313f11219d65
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-21 09:51:58 +0300

    adding mail_user.rb

---
+++ mail_user.rb
@@ -0,0 +1,17 @@
+require "pry"
+class User
+  attr_accessor :email
+
+  def initialize(email_to_save)
+    @email = email_to_save
+    puts "Email : Bienvenue !!"
+  end
+end
+
+binding.pry
+puts "end of file"
+
+julie = User.new("julie@julie.com") #dès le départ, tu rattaches un email à l'instance et affiche un message
+#=> "Email : Bienvenue !!"
+puts julie.email #on vérifie que l'email est bien enregistré. ok, ça retourne => "julie@julie.com"
+jean = User.new #tu vas avoir une erreur car tu as oublié l'email => ArgumentError (wrong number of arguments (given 0, expected 1))
